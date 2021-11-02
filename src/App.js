2cb9c447490b8dbf223ba77f9c3b942855574ccd
batukochan <batukochan@icloud.com>
2021-11-02 20:51:46 +0300

    calisma

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Navbar } from './components/Header/Navbar';
+import {Route} from "react-router-dom"
+import { connect } from 'react-redux';
+import Products from './components/Products';
+import Fav  from './components/fav';
 
-function App() {
+
+function App(props) {
+  // console.log(props.bookList);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Route exact path="/" component={Products}/>
+      <Route path="/fav" component={Fav}/>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    bookList : state.bookList
+  }
+}
+
+export default connect(mapStateToProps)(App);
---
+++ src/components/Header/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Logo from '../../images/logo.png'
+
+export const Navbar = () => {    
+    
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
+                <div className="container">
+                    <a className="navbar-brand" href="/">
+                        <img src={Logo} width={110} alt="logo"></img>
+                    </a>
+                    <div className="col-6 col-xs-2 d-flex">
+                        <div className="input-group">
+                                <input className="form-control border-end-0 border" type="search" placeholder="Kitap veya Yazar Ara" id="example-search-input" />
+                                <span className="input-group-append">
+                                    <button className="btn btn-outline-secondary bg-white border-start-0 border ms-n5" type="button">
+                                        <i className="fa fa-search"></i>
+                                    </button>
+                                </span>
+                            </div>
+                        </div>
+                        <a href="/"><i className="fas fa-user fs-4"></i> </a>
+                </div>
+            </nav>
+        </div>
+    )
+}
